12c3fe92ff9eb5ac8041738641469d4f7ed7ac98
powteng <87842209+powteng@users.noreply.github.com>
2023-05-04 15:09:37 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee2.cxjyk20q7yhf.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "12345678"
+customdb = "employee"
+custombucket = "powzhengteng-bucket"
+customregion = "us-east-1"
+
