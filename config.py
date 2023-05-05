771d7f65aa40cbbe04fb403f8918b8e31f551993
powteng <87842209+powteng@users.noreply.github.com>
2023-05-05 09:28:24 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-customhost = "employee2.cxjyk20q7yhf.us-east-1.rds.amazonaws.com"
+customhost = "employee.cxjyk20q7yhf.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "12345678"
 customdb = "employee"
