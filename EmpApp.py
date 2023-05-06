83d11e134f6a6a14b68a9bb472e1f02788605d4f
powteng <87842209+powteng@users.noreply.github.com>
2023-05-06 11:24:30 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -23,14 +23,14 @@ table = 'employee'
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    #return render_template('AddEmp.html')
-    return render_template('index.html')
+    return render_template('AddEmp.html')
+    #return render_template('index.html')
 
 #@app.route("/about", methods=['POST'])
 #def about():
     #return render_template('About.html')
 
-@app.route('/about', methods=['GET'])
+@app.route("/about", methods=['GET', 'POST'])
 def about():
     return render_template('About.html')
 
