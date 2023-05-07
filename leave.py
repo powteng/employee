18089f74dbb8818fd10fc8d9733fa576bbca986a
powteng <87842209+powteng@users.noreply.github.com>
2023-05-07 18:02:55 +0800

    Create leave.py

---
+++ leave.py
@@ -0,0 +1,35 @@
+from pymysql.cursors import DictCursor
+import os
+import boto3
+from botocore import UNSIGNED
+from botocore.client import Config
+from botocore.handlers import disable_signing
+from config import *
+from db_connection import db_conn, db_close
+
+table = "leave"
+bucket = custombucket
+region = customregion
+
+def add_leave(data):
+    emp_name = data['employee-name']
+    _type = data['leave-type']
+    start_date = data['start-date']
+    end_date = data['end-date']
+    reason = data['reason']
+    
+    try:
+        conn = db_conn()
+        cursor = conn.cursor()
+        insert_sql = "INSERT INTO " + table + " VALUES(%s, %s, %s, %s, %s)"
+        print(insert_sql)
+        cursor.execute(insert_sql, (emp_name, _type, start_date, end_date, reason))
+        conn.commit()
+
+    except Exception as e:
+        return str(e)
+
+    finally:
+        cursor.close()
+        db_close(conn)
+        return '''<script>alert('Form submitted!');</script>'''
