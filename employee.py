8033672a04b873d8f59629c4b7d9b0c72f00b7aa
powteng <87842209+powteng@users.noreply.github.com>
2023-05-07 14:05:11 +0800

    Update employee.py

--- employee.py
+++ employee.py
@@ -33,8 +33,8 @@ def add_employee(fn, ro, em, ge, ph, photo, ic):
     try:
         conn = db_conn()
         cursor = conn.cursor()
-        insert_sql = "INSERT INTO " + table + " VALUES (%s, %s, %s, %s, %s, %s, %s, %s, 0)"
-        cursor.execute(insert_sql, (None, fn, ic, ro, em, ph, ge, s3_photo_key))
+        insert_sql = "INSERT INTO " + table + " (full_name, ic, role, email, phone, gender, s3_photo_key) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        cursor.execute(insert_sql, (fn, ic, ro, em, ph, ge, s3_photo_key))
         conn.commit()
         try:
             print("Data inserted in MySQL RDS... uploading image to S3...")
